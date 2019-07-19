13012b3779991e100cb6a542022ddbbde2b1d169
jyp <jyp10@qq.com>
2019-07-19 14:26:43 +0800

    commit

--- src/main/java/org/jiangyp/kafka/Consumer.java
+++ src/main/java/org/jiangyp/kafka/Consumer.java
@@ -17,7 +17,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 public class Consumer {
 	public static void main(String[] args) {
 		Properties p = new Properties();
-		p.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9092,kafka2:9092,kafka3:9092");
+		p.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.4.109:9092");
 		p.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
 		p.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		p.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer1");
